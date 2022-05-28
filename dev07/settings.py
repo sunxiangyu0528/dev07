462f2dd9bd9eb6c3b0cf09de472486a45f4b5a94
sunxiangyu <1340518678@qq.com>
2022-05-28 21:49:07 +0800

    20210607_httprunner_变量_环境变量_函数_extract_validate_参数化

--- dev07/settings.py
+++ dev07/settings.py
@@ -10,12 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# apps绝对路径
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -45,6 +48,12 @@ INSTALLED_APPS = [
     # 'projects.apps.ProjectsConfig',
     'interfaces',
     'users',
+    'testcases',
+    'configures',
+    'reports',
+    'debugtalks',
+    'testsuites',
+    'envs',
 ]
 
 MIDDLEWARE = [
--- dev07_httprunner/test.py
+++ dev07_httprunner/test.py
@@ -1,6 +1,6 @@
 from httprunner.api import HttpRunner
 
 
-httprun = HttpRunner()
-httprun.run(r'C:\Users\KeYou\PycharmProjects\dev07\dev07_httprunner\api\login_api.yml')
+httprun = HttpRunner(save_tests=True)
+httprun.run(r'C:\Users\KeYou\PycharmProjects\dev07\dev07_httprunner\api\login_api_bk.yml')
 print(httprun._summary)
