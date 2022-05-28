1f7b073f8c8b4bd6eaec0759e44bd90501609124
sunxiangyu <1340518678@qq.com>
2022-05-28 21:27:57 +0800

    第二次课程

--- dev07/settings.py
+++ dev07/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'projects',
+    # 'projects.apps.ProjectsConfig',
+    'interfaces',
 ]
 
 MIDDLEWARE = [
